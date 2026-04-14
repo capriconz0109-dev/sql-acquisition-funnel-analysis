33cb175fdbb74ea35410e7070789b43770669952
Ng Joon Han <capriconz0109@gmail.com>
2026-04-14 20:25:34 +0800

    Add funnel validation query

---
+++ queries/01_funnel_validation.sql
@@ -0,0 +1,14 @@
+SELECT
+  event_name,
+  COUNT(*) AS event_count,
+  COUNT(DISTINCT user_pseudo_id) AS user_count
+FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+WHERE event_name IN ('view_item', 'add_to_cart', 'begin_checkout', 'purchase')
+GROUP BY event_name
+ORDER BY
+  CASE event_name
+    WHEN 'view_item' THEN 1
+    WHEN 'add_to_cart' THEN 2
+    WHEN 'begin_checkout' THEN 3
+    WHEN 'purchase' THEN 4
+  END;
